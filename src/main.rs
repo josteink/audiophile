75c449c5adc770e8120feab9891099b16468f5a2
Jostein Kjønigsen <jostein@ryzen.lan>
2019-03-28 12:47:49 +0100

    Refactor to prepare for WAV support.

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,23 @@ extern crate claxon;
 
 use std::ffi::OsStr;
 
+struct MediaInfo {
+    path: std::string::String,
+    depth: u32,
+    rate: u32
+}
+
 fn main() {
     let root_dir = get_root_dir();
     let mask = format!("{}/**/*.flac", root_dir);
-
-    // let files = glob::glob("../**/*.flac").expect("Error listing files!");
     let files = glob::glob(&mask).expect("Error listing files!");
 
     for item in files {
         let path = item.unwrap();
         if path.is_file() && path.extension() == Some(OsStr::new("flac")) {
-            let (depth, rate) = get_bit_depth_of_flac(&path);
-            if depth > 16 || rate > 44100 {
-                println!("{} ({} - {}Hz)", path.display(), depth, rate);
+            let media_info = get_media_info_for_flac(&path);
+            if media_info.depth > 16 || media_info.rate > 44100 {
+                println!("{} ({} - {}Hz)", media_info.path, media_info.depth, media_info.rate);
             }
         }
     }
@@ -31,9 +35,13 @@ fn get_root_dir() -> std::string::String {
     dir.to_string()
 }
 
-fn get_bit_depth_of_flac(path: &std::path::Path) -> (u32, u32) {
+fn get_media_info_for_flac(path: &std::path::Path) -> MediaInfo {
     let reader = claxon::FlacReader::open(path).unwrap();
     let metadata = reader.streaminfo();
 
-    (metadata.bits_per_sample, metadata.sample_rate)
+    MediaInfo {
+        path: path.display().to_string(),
+        depth: metadata.bits_per_sample,
+        rate: metadata.sample_rate
+    }
 }
