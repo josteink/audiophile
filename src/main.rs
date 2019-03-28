364cacbfcb201080f3521a232c8391cfe3bff893
Jostein Kjønigsen <jostein@kjonigsen.net>
2019-03-28 21:25:54 +0100

    Fix error in output formatting. Move logic to struct.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,63 @@
 extern crate claxon;
 extern crate hound;
 
+use std::path::Path;
+
 struct MediaInfo {
     format: String,
     depth: u32,
     rate: u32
 }
 
+impl MediaInfo {
+    fn is_audiophile_grade_audio(&self) -> bool {
+        self.depth > 16 || self.rate > 44100
+    }
+
+    fn from_path(path: &Path) -> Option<MediaInfo> {
+        let ext = path.extension().unwrap_or_default().to_str().unwrap();
+
+        match ext.to_lowercase().as_str() {
+            "flac" => Some(MediaInfo::from_flac(path)),
+            "wav" => MediaInfo::from_wav(path),
+            _ => None
+        }
+    }
+
+    fn from_flac(path: &Path) -> MediaInfo {
+        let reader = claxon::FlacReader::open(path).unwrap();
+        let metadata = reader.streaminfo();
+
+        MediaInfo {
+            format: "Flac".to_string(),
+            depth: metadata.bits_per_sample,
+            rate: metadata.sample_rate
+        }
+    }
+
+    fn from_wav(path: &Path) -> Option<MediaInfo> {
+        let result = hound::WavReader::open(path);
+
+        match result {
+            Ok(reader) => {
+                let metadata = reader.spec();
+
+                Some(MediaInfo {
+                    format: "Wav".to_string(),
+                    depth: metadata.bits_per_sample as u32,
+                    rate: metadata.sample_rate
+                })
+            },
+            _ => None
+        }
+    }
+}
+
 impl std::fmt::Display for MediaInfo {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let _rate = (self.rate as f32)/1000.0;
 
-        write!(f, "{} - {}-bit, {}kHz)", self.format, self.depth, _rate)
+        write!(f, "{} - {}-bit, {}kHz", self.format, self.depth, _rate)
     }
 }
 
@@ -24,12 +70,12 @@ fn main() {
     for item in files {
         let path = item.unwrap();
         if path.is_file() {
-            let media_info = get_media_info_for(&path);
+            let media_info = MediaInfo::from_path(&path);
 
             match media_info {
                 None => (),
                 Some(info) => {
-                    if is_audiophile_grade_audio(&info) {
+                    if info.is_audiophile_grade_audio() {
                         println!("{} ({})", path.display(), info);
                     }
                 }
@@ -45,46 +91,4 @@ fn get_root_dir() -> std::string::String {
 
     let dir = &args[1];
     dir.to_string()
-}
-
-fn get_media_info_for(path: &std::path::Path) -> Option<MediaInfo> {
-    let ext = path.extension().unwrap_or_default().to_str().unwrap();
-
-    match ext.to_lowercase().as_str() {
-        "flac" => Some(get_media_info_for_flac(path)),
-        "wav" => get_media_info_for_wav(path),
-        _ => None
-    }
-}
-
-fn get_media_info_for_flac(path: &std::path::Path) -> MediaInfo {
-    let reader = claxon::FlacReader::open(path).unwrap();
-    let metadata = reader.streaminfo();
-
-    MediaInfo {
-        format: "Flac".to_string(),
-        depth: metadata.bits_per_sample,
-        rate: metadata.sample_rate
-    }
-}
-
-fn get_media_info_for_wav(path: &std::path::Path) -> Option<MediaInfo> {
-    let result = hound::WavReader::open(path);
-
-    match result {
-        Ok(reader) => {
-            let metadata = reader.spec();
-
-            Some(MediaInfo {
-                format: "Wav".to_string(),
-                depth: metadata.bits_per_sample as u32,
-                rate: metadata.sample_rate
-            })
-        },
-        _ => None
-    }
-}
-
-fn is_audiophile_grade_audio(media_info: &MediaInfo) -> bool {
-    media_info.depth > 16 || media_info.rate > 44100
 }
