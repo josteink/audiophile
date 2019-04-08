cb145c3dc27d4a076b4be828c0f8af769b4b4158
Jostein Kjønigsen <jostein@kjonigsen.net>
2019-04-08 12:11:25 +0200

    Refactor tests.

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -2,36 +2,30 @@ extern crate audiophile;
 
 #[cfg(test)]
 mod tests {
+    use audiophile::MediaInfo;
+
     #[test]
     fn cd_quality_is_not_audiophile() {
-        let testee = audiophile::MediaInfo {
-            format: "test".to_string(),
-            depth: 16,
-            rate: 44100,
-        };
-        
-        assert_eq!(false, testee.is_audiophile_grade_audio());
+        test_audio(false, 16, 44100);
     }
 
     #[test]
     fn bit24_quality_is_audiophile() {
-        let testee = audiophile::MediaInfo {
-            format: "test".to_string(),
-            depth: 24,
-            rate: 44100,
-        };
-        
-        assert_eq!(true, testee.is_audiophile_grade_audio());
+        test_audio(true, 24, 44100);
     }
 
     #[test]
     fn rate48_quality_is_audiophile() {
-        let testee = audiophile::MediaInfo {
+        test_audio(true, 16, 48000);
+    }
+
+    fn test_audio(is_audiophile: bool, depth: u32, rate: u32) {
+        let testee = MediaInfo {
             format: "test".to_string(),
-            depth: 16,
-            rate: 48000,
+            depth: depth,
+            rate: rate,
         };
-        
-        assert_eq!(true, testee.is_audiophile_grade_audio());
+
+        assert_eq!(is_audiophile, testee.is_audiophile_grade_audio());
     }
 }
